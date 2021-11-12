4a9d81f36e261143b107154ca4d40d3b1d28d594
Luka <lukatjee.dev@gmail.com>
2021-11-12 11:26:05 +0100

    Fixed intendation

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -1,26 +1,26 @@
 <?php
 
-    class DBH {
+class DBH {
 
-        protected function connect() {
+    protected function connect() {
 
-            try {
+        try {
 
-                $hostname = "49.12.205.241";
-                $database = "lms";
-                $username = "lms_admin";
-                $password = "oAAXkgtQMOhF8Jo8ozs9Rn5LW2N4zWGfpJwJxbmHS7EpECOv";
-                $charset = "utf8mb4";
+            $hostname = "49.12.205.241";
+            $database = "lms";
+            $username = "lms_admin";
+            $password = "oAAXkgtQMOhF8Jo8ozs9Rn5LW2N4zWGfpJwJxbmHS7EpECOv";
+            $charset = "utf8mb4";
 
-                return new PDO("mysql:host=$hostname;dbname=$database;charset=$charset", $username, $password);
-                
-            } catch (PDOException $e) {
+            return new PDO("mysql:host=$hostname;dbname=$database;charset=$charset", $username, $password);
 
-                print "Couldn't connect to database: " . $e->getMessage() . "<br>";
-                die();
+        } catch (PDOException $e) {
 
-            }
-            
+            print "Couldn't connect to database: " . $e->getMessage() . "<br>";
+            die();
+
         }
-        
+
     }
+
+}
