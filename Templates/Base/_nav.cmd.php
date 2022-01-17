f9806f4ba9f4c58bb500ca0f3eace950cb073664
Luka <lukatjee.dev@gmail.com>
2022-01-17 21:07:58 +0100

    new navigation bar

--- Templates/Base/_nav.cmd.php
+++ Templates/Base/_nav.cmd.php
@@ -1,18 +1,15 @@
-<nav class="navbar navbar-expand-md bg-success navbar-dark">
+<nav class="navbar navbar-expand-md navbar-dark">
 
-    <div class="container">
+    <div class="container bg-danger bg-gradient py-2 px-5">
 
-        <a class="navbar-brand" href="#">
+        <a class="navbar-brand" href="./">
 
             Zorion-LMS
 
         </a>
 
-        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-
+        <button class="navbar-toggler" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
-
         </button>
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -25,10 +22,10 @@
 
             </ul>
 
-            <div class="d-grid gap-2 d-md-block">
+            <div class="d-grid gap-2 d-md-block bg-dark p-2">
 
-                <a href="/Templates/Console/index.php" class="btn btn-dark">Console</a>
-                <a href="/services/signout.serv.php" class="btn btn-danger">Uitloggen</a>
+                <a href="/Templates/Console/index.php" class="btn bg-dark border-secondary text-white rounded-0 shadow-none"><i class="bi bi-terminal"></i></a>
+                <a href="/services/signout.serv.php" class="btn bg-danger text-white rounded-0 shadow-none"><i class="bi bi-box-arrow-right"></i></a>
 
             </div>
 
